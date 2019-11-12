b0f1904ea76d5900feeded2b056028e746885bf9
root <manley louis>
2019-11-12 13:16:50 +0200

    created css and js files

--- index.php
+++ index.php
@@ -1,7 +1,11 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width,initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
 	<title>Home</title>
+	<link rel="styleshee" href="css/main.css">
 </head>
 <body>
 
